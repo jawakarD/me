4c3a02f1d93d6968a580f2fe5a77f6de688948e8
jawakarD <jawakarsmith@gmail.com>
2022-11-06 17:26:58 +0530

    Add social details

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,4 +5,11 @@
 <header class="lg:mt-16 lg:ml-32 m-6">
 	<h1 class="lg:text-7xl text-4xl mb-12">Jawakar Durai</h1>
 	<p class="text-2xl mb-12">A curios boy grown into a man and stayed curious</p>
+
+	<div class="underline flex gap-4">
+		<a href="https://www.linkedin.com/in/jawakar-d/">LinkedIn</a>
+		<a href="https://github.com/jawakarD/">Github</a>
+		<a href="https://letterboxd.com/shindlerr/">Ltterboxd</a>
+		<a href="mailto:jawakarsmith@gmail.com">Email</a>
+	</div>
 </header>
